f87465339e790105d01e88b6300845d9176c5940
edhale3 <edhale33@gmail.com>
2020-11-12 01:32:35 -0800

    Added backend, added proxy from front end to backend, added Postgres setup with knex

---
+++ queries/posts.sql
@@ -0,0 +1,13 @@
+create extension if not exists "uuid-ossp";
+
+create table if not exists posts (
+    posts_id uuid default uuid_generate_v4(),
+    post_title varchar (100) not null,
+    post_price varchar(50) not null,
+    description varchar(1000) not null,
+    image_url varchar(200) not null,
+    category varchar(100) not null,
+    unit_of_rental varchar(100) not null,
+    PRIMARY KEY(posts_id)
+)
+
