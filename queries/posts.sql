668f1bef12461977cadeb96af21b0bdd5193aaf3
edhale3 <edhale33@gmail.com>
2020-11-17 23:59:43 -0800

    Changed post table

--- queries/posts.sql
+++ queries/posts.sql
@@ -5,10 +5,14 @@ create table if not exists posts (
     post_title varchar (100) not null,
     post_price varchar(50) not null,
     description varchar(1000) not null,
+    city_location varchar(300) not null,
+    state_location varchar(300) not null,
+    latitude decimal not null,
+    longitude decimal not null,
     image_url varchar(200) not null,
     category varchar(100) not null,
     unit_of_rental varchar(100) not null,
-    date_of_post timestamp with time zone,
+    date_of_post timestamp with time zone default current_timestamp,
     PRIMARY KEY(posts_id)
 )
 
