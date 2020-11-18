3153d1fb2dcbc52d83b86d2f97d4509f22c2270e
edhale3 <edhale33@gmail.com>
2020-11-18 12:56:43 -0800

    Cleaned up server and added more filtering logic

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 6001
 let bodyParser = require('body-parser');
-let path = require('path')
+let path = require('path');
+// const { filter } = require('async');
 
 const db = require('knex')({
     client:'pg',
@@ -16,81 +17,28 @@ const deps = { db }
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
-
-
-
-// function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
-//     var R = 6371; // Radius of the earth in km
-//     var dLat = deg2rad(lat2-lat1);  // deg2rad below
-//     var dLon = deg2rad(lon2-lon1); 
-//     var a = 
-//       Math.sin(dLat/2) * Math.sin(dLat/2) +
-//       Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-//       Math.sin(dLon/2) * Math.sin(dLon/2)
-//       ; 
-//     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-//     var d = R * c; // Distance in km
-//     return d;
-//   }
-  
-// function deg2rad(deg) {
-//   return deg * (Math.PI/180)
-// }
-
-//latitude: 1 degree = 69 miles
-//          1 mile = 0.01449275 degrees
-/*
-
-5 miles = 0.07246375 degrees
-15 miles = 0.21739125 degrees
-25 miles = 0.36231875 degrees
-50 miles = 0.7246375 degrees
-100 miles = 1.449275 degrees
-250 miles = 3.6231875 degrees
-
-Longitude:
-                    Cosine of current latitude in radians, convert to Miles
-1 degree = (111.32*Math.cos((CurrentLatitude * Math.PI)/180) * .621371)
-1 mile = 1/(111.32*Math.cos((CurrentLatitude * Math.PI)/180) * .621371)
-5 miles = 5/(111.32*Math.cos((CurrentLatitude * Math.PI)/180) * .621371)
-15 miles = 15/(111.32*Math.cos((CurrentLatitude * Math.PI)/180) * .621371)
-25 miles = 25/111.32*(111.32*Math.cos((CurrentLatitude * Math.PI)/180) * .621371)
-50 miles = 50/(111.32*Math.cos((CurrentLatitude * Math.PI)/180) * .621371)
-100 miles = 100/(111.32*Math.cos((CurrentLatitude * Math.PI)/180) * .621371)
-250 miles = 250/(111.32*Math.cos((CurrentLatitude * Math.PI)/180) * .621371)
-
-*/
-
-
-
-
 app.get('/api/posts', async (req, res) => {
     const db = await deps.db
     const filters =  await req.query
     if(Object.keys(filters).length !== 0){
-        console.log("You got here: ", filters)
-        let maxLngEast = await parseFloat(filters.lng) + (filters.radius/(111.32*Math.cos((parseFloat(filters.lat) * Math.PI)/180) * .621371))
-        let maxLngWest = await parseFloat(filters.lng) - (filters.radius/(111.32*Math.cos((parseFloat(filters.lat) * Math.PI)/180) * .621371))
-        let maxLatNorth = await parseFloat(filters.lat) + (filters.radius * 0.01449275)
-        let maxLatSouth = await parseFloat(filters.lat) - (filters.radius * 0.01449275)
-        await console.log(filters)
-        const records = await db('posts')
-            .select()
+        let locationToggle = false;
+        let maxLngEast, maxLngWest, maxLatNorth, maxLatSouth
+        if(filters.radius && filters.lng && filters.lat){
+            maxLngEast = parseFloat(filters.lng) + (filters.radius/(111.32*Math.cos((parseFloat(filters.lat) * Math.PI)/180) * .621371))
+            maxLngWest = parseFloat(filters.lng) - (filters.radius/(111.32*Math.cos((parseFloat(filters.lat) * Math.PI)/180) * .621371))
+            maxLatNorth = parseFloat(filters.lat) + (filters.radius * 0.01449275)
+            maxLatSouth = parseFloat(filters.lat) - (filters.radius * 0.01449275)
+            locationToggle = true
+        }
+        const records = await db('posts').select()
             .where(function(){
-                if(filters.unit){
-                    this.where('unit_of_rental',`${filters.unit}`)
-                } else {
-                    this.whereNot('unit_of_rental','none')
-                }
+                filters.unit ? this.where('unit_of_rental',`${filters.unit}`) : this.whereNot('unit_of_rental','none')
             })
-            .andWhere('longitude','>',`${filters.lng !== undefined? maxLngWest : -180}`)
-            .andWhere('longitude','<',`${filters.lng !== undefined? maxLngEast : 180}`)
-            .andWhere('latitude','<',`${filters.lat !== undefined? maxLatNorth : 90}`)
-            .andWhere('latitude','>',`${filters.lat !== undefined? maxLatSouth : -90}`)
-            .catch(err => {
-                throw new Error(err)
-            })
-            await console.log(records)
+            .andWhere('longitude','>',`${locationToggle? maxLngWest : -180}`)
+            .andWhere('longitude','<',`${locationToggle? maxLngEast : 180}`)
+            .andWhere('latitude','<',`${locationToggle? maxLatNorth : 90}`)
+            .andWhere('latitude','>',`${locationToggle? maxLatSouth : -90}`)
+            .catch(err => { throw new Error(err)})
         return res.json(records)
     } else {
         const records = await db('posts').select()
@@ -122,7 +70,6 @@ app.post('/api/post', async (req,res) => {
     res.send("Success")
 })
 
-
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
     app.use(express.static(path.join(__dirname, 'client/build')));
@@ -132,8 +79,33 @@ if (process.env.NODE_ENV === 'production') {
     });
   }
 
-
-
 app.listen(port, ()=> [
     console.log(`Congratulations you are now listening on port:${port} `)
 ])
+
+
+
+
+//latitude: 1 degree = 69 miles
+//          1 mile = 0.01449275 degrees
+/*
+
+5 miles = 0.07246375 degrees
+15 miles = 0.21739125 degrees
+25 miles = 0.36231875 degrees
+50 miles = 0.7246375 degrees
+100 miles = 1.449275 degrees
+250 miles = 3.6231875 degrees
+
+Longitude:
+                    Cosine of current latitude in radians, convert to Miles
+1 degree = (111.32*Math.cos((CurrentLatitude * Math.PI)/180) * .621371)
+1 mile = 1/(111.32*Math.cos((CurrentLatitude * Math.PI)/180) * .621371)
+5 miles = 5/(111.32*Math.cos((CurrentLatitude * Math.PI)/180) * .621371)
+15 miles = 15/(111.32*Math.cos((CurrentLatitude * Math.PI)/180) * .621371)
+25 miles = 25/111.32*(111.32*Math.cos((CurrentLatitude * Math.PI)/180) * .621371)
+50 miles = 50/(111.32*Math.cos((CurrentLatitude * Math.PI)/180) * .621371)
+100 miles = 100/(111.32*Math.cos((CurrentLatitude * Math.PI)/180) * .621371)
+250 miles = 250/(111.32*Math.cos((CurrentLatitude * Math.PI)/180) * .621371)
+
+*/
